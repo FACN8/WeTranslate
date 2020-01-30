a337890957b473e07625738fdddb346abdf42de0
Francis Aboud <francisaboud2000@gmail.com>
2020-01-30 12:44:13 +0200

    fixes #12

--- public/index.js
+++ public/index.js
@@ -1,49 +1,51 @@
 const inp = document.getElementById("search");
-const transResult = document.getElementById('translated')
-const myHandler = function () { if (inp.value.length === 0) { transResult.value = '' } else { translate(); }; }
+const transResult = document.getElementById("translated");
+const myHandler = function() {
+  if (inp.value.length === 0) {
+    transResult.value = "";
+  } else {
+    translate();
+  }
+};
 const dHandler = debounced(200, myHandler);
 inp.addEventListener("input", dHandler);
 
 function translate() {
-    let searchVal = inp.value
-    let fromLang = document.getElementById('from').value
-    let toLang = document.getElementById('to').value
-    postRequest({ searchVal, fromLang, toLang }, '/search', (error, response) => {
-        if (error) {
-            console.log(error, 'No Error');
-        }
-        else if (response.status === 200) {
-            console.log('the API should respond with a status code of 200')
-            let translated = response.data.translations[0].translation
-            transResult.value = translated;
-            mode = false;
-            console.log(translated)
-        } else { console.log('our server error') }
-
-    })
+  let searchVal = inp.value;
+  let fromLang = document.getElementById("from").value;
+  let toLang = document.getElementById("to").value;
+  postRequest({ searchVal, fromLang, toLang }, "/search", (error, response) => {
+    if (error) {
+        transResult.value = "something went wrong please contact us";
+    } else if (response.status === 200) {
+      let translated = response.data.translations[0].translation;
+      transResult.value = translated;
+        } else {
+      transResult.value = "something went wrong please contact us";
+    }
+  });
 }
 
 const postRequest = (body, url, cb) => {
-
-    axios.post(url, body)
-        .then((result) => {
-
-            cb(null, result)
-        })
-        .catch((error) => {
-            cb(error)
-        })
+  axios
+    .post(url, body)
+    .then(result => {
+      cb(null, result);
+    })
+    .catch(error => {
+      cb(error);
+    });
 };
 
 function debounced(delay, fn) {
-    let timerId;
-    return function (...args) {
-        if (timerId) {
-            clearTimeout(timerId);
-        }
-        timerId = setTimeout(() => {
-            fn(...args);
-            timerId = null;
-        }, delay);
+  let timerId;
+  return function(...args) {
+    if (timerId) {
+      clearTimeout(timerId);
     }
+    timerId = setTimeout(() => {
+      fn(...args);
+      timerId = null;
+    }, delay);
+  };
 }
